3d5213f43c34ae888ee7c3aa75932d03fc53f842
pedro-alten-machine <pedrosilvamnchunks@gmail.com>
2024-06-27 18:34:37 +0200

    Server stream example done with tests and everything

--- java-grpc/app/src/main/java/serverImpl/ClientServerStreamImpl.java
+++ java-grpc/app/src/main/java/serverImpl/ClientServerStreamImpl.java
@@ -1,5 +1,23 @@
 package serverImpl;
 
-public class ClientServerStreamImpl {
-    
+import io.grpc.myexamples.serverstream.ReplyServerStream;
+import io.grpc.myexamples.serverstream.ReqServerStream;
+import io.grpc.myexamples.serverstream.ServerStreamGrpc.ServerStreamImplBase;
+import io.grpc.stub.StreamObserver;
+
+public class ClientServerStreamImpl extends ServerStreamImplBase {
+    // ? This one simply receives one message and sends all of the numbers by a
+    // stream observer
+    @Override
+    public void requestThrowOfNumbers(ReqServerStream req, StreamObserver<ReplyServerStream> reply) {
+        String message = req.getMessage();
+        System.out.println("Just received the message: " + message);
+        for (int i = 0; i < 5; i++) {
+            reply.onNext(
+                    ReplyServerStream.newBuilder()
+                            .setNumber(i)
+                            .build());
+        }
+        reply.onCompleted();
+    }
 }
