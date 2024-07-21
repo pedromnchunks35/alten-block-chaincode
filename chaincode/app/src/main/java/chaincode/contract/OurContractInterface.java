18bfcd662d9f1caecd34b4b83d2679ff79ac6ca3
pedro-alten-machine <pedrosilvamnchunks@gmail.com>
2024-07-21 02:37:29 +0200

    Done, we must test it only

--- chaincode/app/src/main/java/chaincode/contract/OurContractInterface.java
+++ chaincode/app/src/main/java/chaincode/contract/OurContractInterface.java
@@ -1,7 +1,14 @@
 package chaincode.contract;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.List;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
@@ -13,10 +20,14 @@ import chaincode.exceptions.UserAlreadyExistsException;
 import chaincode.exceptions.UserDoesNotExistException;
 
 public interface OurContractInterface extends ContractInterface {
-    public void createUser(Context ctx, String username, List<List<Integer>> table)
-            throws UserAlreadyExistsException, InvalidInputException, InvalidCertificate, IOException;
+        public void createUser(Context ctx, String username, List<List<Integer>> table)
+                        throws UserAlreadyExistsException, InvalidInputException, InvalidCertificate, IOException;
 
-    public BiometricsDataInterface getUser(Context ctx, String username)
-            throws UserDoesNotExistException, InvalidInputException, InvalidCertificate, IOException,
-            ClassNotFoundException;
+        public BiometricsDataInterface getUser(Context ctx, String username)
+                        throws UserDoesNotExistException, InvalidInputException, InvalidCertificate, IOException,
+                        ClassNotFoundException;
+
+        public String requestTicket(Context ctx, String username)
+                        throws IOException, InvalidCertificate, InvalidKeySpecException, NoSuchAlgorithmException,
+                        InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException;
 }
